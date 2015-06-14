751ed18f14b14540f47ab1fe631417f8e251d891
Salman Sadiq <salman.sadiq@gmail.com>
2015-06-15 00:47:37 +0100

    options object built from commandline params

--- index.js
+++ index.js
@@ -2,43 +2,48 @@
 
 'use strict';
 
-//var phantomasui = require('..');
 var _ = require('lodash');
 var debug = require('debug')('phantomas-ui:cli');
 var logger = require('./lib/logger');
 var cliOptions = require ('minimist')(process.argv.slice(2));
 
+
 var PhantomasUI = function () {}
 
-PhantomasUI.prototype.getOptionsFromCommandline = function () {
-  //!_.isEmpty(cliOptions) &&
-  return _.omit(cliOptions, '_');
+PhantomasUI.prototype.getOptionsFromCommandline = function (cliOptions) {
+  var options = {};
+  var assertions = {};
+
+  _.map(cliOptions, function(value, key) {
+    if (key.match('assert')){
+      assertions[key.split('-')[1]] = value;
+    }
+  });
+
+  options.url = cliOptions.url;
+  options.path = cliOptions.path;
+  options.assertions = assertions;
+
+  return options;
 }
 
-PhantomasUI.prototype.run = function (options, cb) {
+PhantomasUI.prototype.run = function (options) {
 
-  var cmdOptions = this.getOptionsFromCommandline();
-
-  if (!cmdOptions.url) {
+  if (!options.url || options.url === true) {
     logger
       .purpose('\nGraphical user interface for phantomas, based on phantomjs\n\nphantomas-ui <url> [options]\n')
-      .describe ('url', 'String <required>', 'Enter URL you want to collect performance metrics for e.g; --url http://domain.com')
-      .describe ('[options]', 'String <optional>', 'WIP')
-      .describe ('help', '', 'Show full list of options')
-
+      .describe ('url',       'String <required>',  'Enter URL you want to collect performance metrics for e.g; --url http://domain.com/')
+      .describe ('[options]', 'String <optional>',  'WIP')
+      .describe ('help',      '',                   'Show full list of options')
       .printDescribes();
   }
+
 
 }
 
 var phantomasui = new PhantomasUI();
-phantomasui.run();
+var options = phantomasui.getOptionsFromCommandline(cliOptions);
+phantomasui.run(options);
 
 module.exports = PhantomasUI;
 
-
-//check where the script is being called from
-  //if called from command line and has options object
-     //use options array
-  //else
-     //use instantiated options object
